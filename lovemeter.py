af755fc846e5b3e7df26edcdc93c65b0edf1d66a
Borlp <95509079+Borlp@users.noreply.github.com>
2022-02-22 15:45:07 +0100

    Update lovemeter.py

--- lovemeter.py
+++ lovemeter.py
@@ -1,10 +1,2 @@
-from random import randint
-count = randint(100000,1000000)
-indianojonsisan = 0
-for i in range(count):
-    indianojonsisan +=1
-    print(indianojonsisan)
-for s in range(count):
-    indianojonsisan -=1
-    print(indianojonsisan)
+ifimportpass
         
